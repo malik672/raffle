be7d7e4df120addafed8e8ed7a4a682e3973a653
malik672 <aremumalik01@gamil.com>
2022-12-11 19:36:10 +0100

    first

---
+++ src/RaffluxMain.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.8 ;
+
+import "RafluxStorage.sol";
+
+//this the main contract in the raffle
+contract RaffluxMain is RafluxStorage {
+
+//   //STRUCT
+//   //struct of proposal
+//   struct Proposal{
+
+//   }
+//   //MAPPING
+//   // Mapping from user address to the list of staked tokens for that user
+//   mapping(address => uint256[]) public stakedTokens;
+
+//   // Mapping from token ID to the address of the user who listed it
+//   mapping(uint256 => address) public stakedTokenOwners;
+
+//   // Mapping of uint256 to the address of the user who
+//   mapping(address => mapping(uint256 => uint256)) timeLeft;
+
+//  //FUNCTIONS
+
+//  //list the nft
+//  function list(address _tokenAddress, uint _tokenId, uint _time){
+//     //list the nft
+//     RaffluxStorage.depositNft(_tokenAddress, _tokenId);
+//  }
+
+//  //timeleft
+//  function timeLeft() public {
+
+//  }
+
+}
--- src/RafluxStorage.sol
+++ src/RafluxStorage.sol
@@ -1,22 +1,110 @@
 pragma solidity ^0.8;
 
-
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
 
+interface ITest {
+    function isERC1155(address nftAddress) external returns (bool);
+
+    function isERC721(address nftAddress) external returns (bool);
+}
+
+// RafluxStorage is a contract that can store ERC-1155 and ERC-721 tokens,
+// and track the number of points that each address has.
 contract RafluxStorage {
-  //State Variables
-  address public caller;
+    using ERC165Checker for address;
 
-  //mappings
-  mapping(address => uint256) points;
+    //State Variables
+    // - caller: the address of the caller of the contract
+    // - IID_ITEST: the interface ID of the ITest interface
+    // - IID_IERC165: the interface ID of the IERC165 interface
+    // - IID_IERC1155: the interface ID of the IERC1155 interface
+    // - IID_IERC721: the interface ID of the IERC721 interface
+    address public caller;
+    bytes4 public constant IID_ITEST = type(ITest).interfaceId;
+    bytes4 public constant IID_IERC165 = type(IERC165).interfaceId;
+    bytes4 public constant IID_IERC1155 = type(IERC1155).interfaceId;
+    bytes4 public constant IID_IERC721 = type(IERC721).interfaceId;
 
+    // A mapping to track the number of points that each address has.
+    mapping(address => uint256) points;
 
-  constructor(address _caller) payable {
-    caller = _caller;
-  }
+    //constructor
+    constructor() payable {}
 
-  //functions
-  function transfer() public {
+    //EVENTS
+    // - addPoint: emitted when points are added to an address
+    // - removePoint: emitted when points are removed from an address
+    event addPoint(address indexed _user, uint256 _points);
+    event removePoint(address indexed _user, uint256 _points);
 
-  } 
+    //FUNCTIONS
+    //check if the token is an erc721 token
+    function isERC721(address nftAddress) public view returns (bool) {
+        return nftAddress.supportsInterface(IID_IERC721);
+    }
+
+    //check if the token is erc1155 token
+    function isERC1155(address nftAddress) public view returns (bool) {
+        return nftAddress.supportsInterface(IID_IERC1155);
+    }
+
+    //MODIFIER
+    //checks if points is zero
+    modifier checkZero(uint256 _points) {
+        require(_points != 0, "can't be zero");
+        _;
+    }
+
+    //transfer nft from user to contract
+    function depositNft(address _tokenAddress, uint256 _tokenId) public {
+        if (isERC721(_tokenAddress)) {
+            IERC721 Token = IERC721(_tokenAddress);
+            Token.approve(address(this), _tokenId);
+            Token.safeTransferFrom(msg.sender, address(this), _tokenId);
+        } else if (isERC1155(_tokenAddress)) {
+            IERC1155 Tokens = IERC1155(_tokenAddress);
+            //   Tokens.safeTransferFrom(msg.sender, address(this), _tokenId);
+        }
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        returns (bool)
+    {
+        return interfaceId == IID_ITEST || interfaceId == IID_IERC165;
+    }
+
+    // viewPoints returns the number of points that the given address has.
+    function viewPoints(address _user) public view returns (uint256) {
+        return points[_user];
+    }
+
+    // updatePoints updates the number of points that the given address has.
+    function updatePoints(
+        address _user,
+        uint256 _points,
+        bool _bool
+    ) public checkZero(_points) {
+        //if bool is true add to user points else subtract from user points
+        if (_bool) {
+            points[_user] += _points;
+            emit addPoint(_user, _points);
+        } else {
+            emit removePoint(_user, viewPoints(_user));
+            points[_user] -= viewPoints(_user);
+        }
+    }
+
+    //withdraw nft from user to contract
+    function withdrawNft(address _tokenAddress, uint256 _tokenId) public {
+        if (isERC721(_tokenAddress)) {
+            IERC721 Token = IERC721(_tokenAddress);
+            Token.safeTransferFrom(msg.sender, address(this), _tokenId);
+        }
+    }
 }
