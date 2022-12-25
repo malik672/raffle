dc93ae396f912119eab02c41b3efdaefab73a789
malik672 <aremumalik01@gmail.com>
2022-12-25 21:02:01 +0100

    test

--- test/Rafflux.t.sol
+++ test/Rafflux.t.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import {RafluxStorage} from "../src/RafluxStorage.sol";
+import "forge-std/Vm.sol";
+import {RaffluxDao} from "../src/RaffluxDao.sol";
 
 //this a custom nft created on the polygon testnet used to test this
 interface myNfts {
@@ -12,16 +13,16 @@ interface myNfts {
 }
 
 contract RaffluxTest is Test {
-    RafluxStorage Storage;
+    RaffluxDao rafflux;
     address public address1;
     address public address2;
     using stdStorage for StdStorage;
     address constant myAddress = 0x30bE4D758d86cfb1Ae74Ae698f2CF4BA7dC8d693;
     myNfts punks = myNfts(0x2690080D83460264424BcAB8ceE6afFAbCD6b933);
 
-    // RafluxStorage Rafflux = RafluxStorage();
+    
     function setUp() public {
-        Storage = new RafluxStorage();
+        rafflux = new RaffluxDao();
     }
 
     function writeTokenBalance(address who, address token, uint256 amt) internal {
@@ -32,44 +33,39 @@ contract RaffluxTest is Test {
           .checked_write(amt);
     }
 
-    //checks whether it mints
-    function testRed() public {
-      vm.prank(myAddress);
+    //checks whether it starts a raffle
+    function testProposal() public {
+      vm.startPrank(myAddress);
       writeTokenBalance(myAddress, address(punks), 0);
-      // emit log_named_uint("Current ether balance of myAddress", myAddress.balance);
-      // emit log_named_uint("Dai balance of myAddress before", punks.balanceOf(myAddress));
-      // emit log_named_uint("Dai balance of myAddress before", punks.balanceOf(myAddress));
+      rafflux.proposeRaffle('testing a raffle', myAddress, block.timestamp, block.timestamp + 2000, 10, 1, address(punks), 4, 1 ether);
       vm.stopPrank();
     }
 
     
     function testDeposit() public {
-      vm.stopPrank();
-      vm.startPrank();
-      vm.prank(0x30bE4D758d86cfb1Ae74Ae698f2CF4BA7dC8d693);
+      vm.startPrank(myAddress);
       assertGt(punks.balanceOf(myAddress), 1);
       assertEq(punks.ownerOf(4), myAddress);
       // assertNotEq(address(Storage), address(0));
-      emit log_named_address("Current address of sender", msg.sender);
-      punks.approve(address(Storage), 4);
+      // punks.approve(address(Storage), 4);
       // Storage.depositNft(address(punks), 4, 0);
       vm.stopPrank();
     }
 
-    // //test points 
-    // function testUpdatePointExpectedRevert() public {
-    //   // vm.expectRevert("can't be zero");
-    //   vm.prank(Storage.owner());
-    //   /*since the update functions and other mutable functions cann only be called by the owner, we will have to transfer ownership  */
-    //   Storage.updatePoints(myAddress, 10, 0,  true);
-    //   //check it add points
-    //   assertEq(Storage.viewPoints(myAddress, 0), 10);
-    //   //check it removes points
-    //   //the two(2) here is useless since the remove points remove all points a user has and equate it to zero
-    //   Storage.updatePoints(myAddress, 10,0, false);
-    //   // //check if point equal to zero after removal
-    //   assertEq(Storage.viewPoints(myAddress,0), 0);      
-    // }
+    //test points 
+    function testUpdatePointExpectedRevert() public {
+      // vm.expectRevert("can't be zero");
+      vm.prank(Storage.owner());
+      /*since the update functions and other mutable functions cann only be called by the owner, we will have to transfer ownership  */
+      Storage.updatePoints(myAddress, 10, 0,  true);
+      //check it add points
+      assertEq(Storage.viewPoints(myAddress, 0), 10);
+      //check it removes points
+      //the two(2) here is useless since the remove points remove all points a user has and equate it to zero
+      Storage.updatePoints(myAddress, 10,0, false);
+      // //check if point equal to zero after removal
+      assertEq(Storage.viewPoints(myAddress,0), 0);      
+    }
 
     // function testDeposit() public {};
 }
